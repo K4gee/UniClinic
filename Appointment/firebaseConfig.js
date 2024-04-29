711ee7c2cae10cfc4a94c54eb9cfb898fb1888a5
Kacie Ortiz <152044631+K4gee@users.noreply.github.com>
2024-04-29 19:44:48 +0800

    Add files via upload

---
+++ Appointment/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyChYefBVwXO8gPmekTDJtXXm8AB7A92UBk",
+
+    authDomain: "appointmentdb-ed881.firebaseapp.com",
+
+    projectId: "appointmentdb-ed881",
+
+    storageBucket: "appointmentdb-ed881.appspot.com",
+
+    messagingSenderId: "1053862309539",
+
+    appId: "1:1053862309539:web:dac1097a3385d3f5881916",
+
+    measurementId: "G-CCSD7LSF9Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
